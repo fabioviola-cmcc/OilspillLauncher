c5799fdb59c65db5c8a680f58e6e5f3c5f72000f
Fabio Viola <fabio.viola@cmcc.it>
2020-08-03 11:27:42 +0200

    Code cleaned

--- OilspillLauncher.sh
+++ OilspillLauncher.sh
@@ -7,7 +7,16 @@
 # - the submission string (through -s)
 # - the callback url (through -c)
 #
+# Code refactoring performed on: 2020/08/03
 
+
+####################################################
+#
+# Initial setup
+#
+####################################################
+
+APPNAME=$0
 
 
 ####################################################
@@ -26,7 +35,7 @@ function send_to_callback {
                 counter=1
                 while [ $counter -lt 4 ]; do
                         sleep 2
-                        echo -e "`date`\tERROR\tDNS error; attempt #${counter}" 1>&2
+                        echo -e "[$APPNAME] -- `date`\tERROR\tDNS error; attempt #${counter}" 1>&2
                         curl -F "outcode=${f_outcode}" ${f_callback}
                         ret_value=$?
                         if [ $ret_value -ne 6 ]; then
@@ -35,7 +44,7 @@ function send_to_callback {
                         let counter=counter+1
                 done
                 if [ $counter -eq 4 ]; then
-                        echo -e "`date`\tERROR\tDNS error; unable to contact ${f_callback}" 1>&2
+                        echo -e "[$APPNAME] -- `date`\tERROR\tDNS error; unable to contact ${f_callback}" 1>&2
                 fi
         fi
         return $ret_value
@@ -61,7 +70,7 @@ function send_files_to_callback {
                 counter=1
                 while [ $counter -lt 4 ]; do
                         sleep 2
-                        echo -e "`date`\tERROR\tDNS error; attempt #${counter}" 1>&2
+                        echo -e "[$APPNAME] -- `date`\tERROR\tDNS error; attempt #${counter}" 1>&2
         		curl -F "outcode=0" -F "data1=@$f_OutputDir/result.json;type=text/csv" ${f_callback}
                         #curl -F "outcode=0" -F "data1=@$f_OutputDir/$f_queryID/2_voyageplan.json;type=text/csv" -F "data2=@$f_OutputDir/$f_queryID/2_gdt_voyageplan.json;type=text/csv" ${f_callback}
                         ret_value=$?
@@ -71,7 +80,7 @@ function send_files_to_callback {
                         let counter=counter+1
                 done
                 if [ $counter -eq 4 ]; then
-                        echo -e "`date`\tERROR\tDNS error; unable to contact ${f_callback}" 1>&2
+                        echo -e "[$APPNAME] -- `date`\tERROR\tDNS error; unable to contact ${f_callback}" 1>&2
                 fi
         fi
         return $ret_value
@@ -115,7 +124,7 @@ subm_string=$(echo "${subm_string}" | sed 's/[^A-Za-z0-9_:.,;=\^§-]//g')
 
 # Check if callback url is valid
 if [ "$callback_url" == "" ]; then
-        message=$(echo -e "`date`\tERROR\tError in retrieving callback URL")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tError in retrieving callback URL")
         end_time=`date +'%F %T'`
         echo -e $message
         mysql -u root -D dss_job -f -e "insert into job (query_id, dss_name, start_time,end_time,status,exit_code, launcher_err) values (-1, '${userName}', '${start_time}', '${end_time}', 'failed', -1, '${message}')"
@@ -124,7 +133,7 @@ fi
 
 # Check if the query ID is valid
 if [ "$queryID" == "" ]; then
-        message=$(echo -e "`date`\tERROR\tError in retrieving queryID")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tError in retrieving queryID")
         #curl -X POST --data "outcode=-1" ${callback_url}
         end_time=`date +'%F %T'`
         echo -e $message
@@ -136,7 +145,7 @@ fi
 
 # Check if the submission string is valid
 if [ "$subm_string" == "" ]; then
-        message=$(echo -e "`date`\tERROR\tError in retrieving submission string")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tError in retrieving submission string")
         #curl -X POST --data "outcode=-1" ${callback_url}
         end_time=`date +'%F %T'`
         echo -e $message
@@ -158,7 +167,7 @@ export LD_LIBRARY_PATH=/users/home/tessa_gpfs1/opt/netcdf_intel/netcdf-4.3.2/ins
 
 # Clean medslik home before starting
 if [ -d  $HOME_MEDSLIK ];then
-  echo "An old dir already exist I'm deleting old files inside it!"
+  echo "[$APPNAME] -- An old dir already exist I'm deleting old files inside it!"
   rm -rf ${HOME_MEDSLIK}/*
 fi
 mkdir -p ${HOME_MEDSLIK}
@@ -169,19 +178,19 @@ cp -r witoil ${HOME_MEDSLIK}/
 cd $HOME_MEDSLIK
 cp ${MEDSLIK}/oilsp_vs_uncert.sh .
 ls
-echo $OPTARG
+echo "[$APPNAME] -- $OPTARG"
 
 # Invoke oilspill_vs_uncert.sh
 ./oilsp_vs_uncert.sh $queryID > select.out 2>select.err # >> medslic_II.log
 excode=$?
-echo -e "srun EXIT CODE: $excode"
+echo -e "[$APPNAME] -- srun EXIT CODE: $excode"
 
 # Check the exit code of oilspill_vs_uncert
 if [ $excode -ne 0 ]; then
 
     # job cancelled
     if [ $excode -eq 15 ]; then
-        message=$(echo -e "`date`\tERROR\tJob has been cancelled")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tJob has been cancelled")
         end_time=`date +'%F %T'`
         echo -e $message
         ## Creating log file for cancelled job
@@ -196,7 +205,7 @@ if [ $excode -ne 0 ]; then
     # errors in algorithm execution
     elif [ $excode -gt 50 ];then
         excode=51
-	message=$(echo -e "`date`\tERROR\t `cat medslik_II.error`")
+	message=$(echo -e "[$APPNAME] -- `date`\tERROR\t `cat medslik_II.error`")
         end_time=`date +'%F %T'`
         echo -e $message
         mysql -u root -D dss_job -f -e "update job set end_time='${end_time}', status='failed', exit_code=$excode, launcher_err='${message}', algo_err='${message}' where id=${db_dss_id}"
@@ -204,7 +213,7 @@ if [ $excode -ne 0 ]; then
         exit $excode
     elif [ $excode -gt 40 ];then
 	excode=41
-	message=$(echo -e "`date`\tERROR\t `cat medslik_II.error`")
+	message=$(echo -e "[$APPNAME] -- `date`\tERROR\t `cat medslik_II.error`")
         end_time=`date +'%F %T'`
         echo -e $message
         mysql -u root -D dss_job -f -e "update job set end_time='${end_time}', status='failed', exit_code=$excode, launcher_err='${message}', algo_err='${message}' where id=${db_dss_id}"
@@ -212,7 +221,7 @@ if [ $excode -ne 0 ]; then
         exit $excode
     elif [ $excode -gt 30 ];then
 	excode=31
-	message=$(echo -e "`date`\tERROR\t `cat medslik_II.error`")
+	message=$(echo -e "[$APPNAME] -- `date`\tERROR\t `cat medslik_II.error`")
         end_time=`date +'%F %T'`
         echo -e $message
         mysql -u root -D dss_job -f -e "update job set end_time='${end_time}', status='failed', exit_code=$excode, launcher_err='${message}', algo_err='${message}' where id=${db_dss_id}"
@@ -220,14 +229,14 @@ if [ $excode -ne 0 ]; then
         exit $excode
     elif [ $excode -gt 20 ];then
 	excode=21
-	message=$(echo -e "`date`\tERROR\t `cat medslik_II.error`")
+	message=$(echo -e "[$APPNAME] -- `date`\tERROR\t `cat medslik_II.error`")
         end_time=`date +'%F %T'`
         echo -e $message
         mysql -u root -D dss_job -f -e "update job set end_time='${end_time}', status='failed', exit_code=$excode, launcher_err='${message}', algo_err='${message}' where id=${db_dss_id}"
         send_to_callback ${excode} ${callback_url}
         exit $excode
     else
-        message=$(echo -e "`date`\tERROR\tError in algorithm execution")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tError in algorithm execution")
         end_time=`date +'%F %T'`
         echo -e $message
         #algo_err=$(head -c 1024 $CodeCDAMDir/$queryID/algo.err|tr "\n" " ")
@@ -252,7 +261,7 @@ zip -j ${Id_Dir}.zip ${Id_Dir}/*
 mv ${Id_Dir}.zip ${Id_Dir}/
 scp -r $Id_Dir ${userName}@193.204.199.175:/var/www/html/${userName}/
 if [ $? -ne 0 ]; then
-        message=$(echo -e "`date`\tERROR\tError in sending the tiles")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tError in sending the tiles")
         end_time=`date +'%F %T'`
         echo -e $message
         #algo_err=$(head -c 1024 $CodeCDAMDir/$queryID/algo.err|tr "\n" " ")
@@ -267,7 +276,7 @@ echo -e "{\n\t\"files\": [">result.json; for i in $(ls scorners.json); do echo -
 # invoking send_files_to_callback
 send_files_to_callback $OutputDir $queryID ${callback_url}
 if [ $? -ne 0 ]; then
-        message=$(echo -e "`date`\tERROR\tError in sending the output files")
+        message=$(echo -e "[$APPNAME] -- `date`\tERROR\tError in sending the output files")
 	end_time=`date +'%F %T'`
         echo -e $message
 	mysql -u root -D dss_job -f -e "update job set end_time='${end_time}', status='failed', exit_code=-3, launcher_err='${message}' where id=${db_dss_id}"
@@ -278,4 +287,5 @@ fi
 # update mysql entry and exit succesfully
 end_time=`date +'%F %T'`
 mysql -u root -D dss_job -f -e "update job set end_time='${end_time}', status='success', exit_code=0 where id=${db_dss_id}"
+echo "[$APPNAME] -- Elaboration completed!"
 exit 0
--- jsonToInput.sh
+++ jsonToInput.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-## This file take in input a json string and create two files: 
-##"medslik_inputfile.txt" and "medslik5.par" to run medslik_II
-## --------------- Set variables and exit codes for error management ---------------##
+#
+# === jsonToInput.sh ===
+#
+# This file take in input a json string and create two files: 
+# "medslik_inputfile.txt" and "medslik5.par" to run medslik_II
+#
+# Code refactoring performed on: 2020/08/03
+
+
+####################################################
+#
+# Set variables and exit codes for error management
+#
+####################################################
+
+APPNAME=$0
 userName=`whoami`
 export homeDir="/users/home/tessa_gpfs1/${userName}"
 workDir=/work/tessa_gpfs2/${userName}/OILSPILL_DA/out
@@ -10,10 +23,18 @@ export INP_BADARGS="-1"   # code: 255
 export Curr_BADARGS="-2"  # code: 254
 export Wind_BADARGS="-3"  # code: 253
 export SIM_DUR_ARGS="-4"  # code: 252
-## --------------- Check for the number of parameters otherwise exit ---------------##
+
+
+####################################################
+#
+# Read and check command line arguments
+#
+####################################################
+
+# Check for the number of parameters otherwise exit
 if [ $# -ne 1 ] ; then
-   echo -e " --- `date ` There must be one input parameter as a string \n "
-   echo -e "Usage: ./`basename $0` 'sim_name=simname;model=MYO1h;wind=ECMWF05;sim_length=0024;day=15;month=10;year=13;hour=08;minutes=00;lat_degree=42;lat_mintes=22.28;lon_degree=10;lon_minutes=55.50;duration=0036;spillrate=00006.80;oil=API;oiltype=17;var_2=1;var_3=0.00;var_10=2.0;var_14=90000;var_19=0.0000033;var_29=0.000008;var_39=150' \n"
+   echo -e "[$APPNAME] -- `date ` There must be one input parameter as a string \n "
+   echo -e "[$APPNAME] -- Usage: ./`basename $0` 'sim_name=simname;model=MYO1h;wind=ECMWF05;sim_length=0024;day=15;month=10;year=13;hour=08;minutes=00;lat_degree=42;lat_mintes=22.28;lon_degree=10;lon_minutes=55.50;duration=0036;spillrate=00006.80;oil=API;oiltype=17;var_2=1;var_3=0.00;var_10=2.0;var_14=90000;var_19=0.0000033;var_29=0.000008;var_39=150' \n"
    exit $INP_BADARGS
 fi
 inputString=$1
@@ -21,8 +42,16 @@ idDir=`echo ${inputString} | awk -F';' '{for(i = 1;i <= NF;i++){printf "%s\n", $
 inputString=${inputString//\`/xxx}
 workingDir=${workDir}/${idDir}/witoil
 cd ${workingDir}
-echo "I'm in directory ${workingDir}"
+echo "[$APPNAME] -- I'm in directory ${workingDir}"
 echo $1 > infile.txt
+
+####################################################
+#
+# parse input string
+#
+####################################################
+
+echo "[$APPNAME] -- Parsing input string..."
 echo "${inputString}" | awk -F';' '{for(i = 1;i <= NF;i++){printf "%s\n", $i}}' | while read LINE; do # >>${workingDir}/file.txt # | while read LINE; do 
 	case $LINE in
 		"selector="*)
@@ -143,38 +172,58 @@ echo "${inputString}" | awk -F';' '{for(i = 1;i <= NF;i++){printf "%s\n", $i}}' 
 			echo "export HOUR_STEP=${Content}" >> hour_step.sh
 		;;
                 *)
-			echo "$LINE Input parameter not known"
+			echo "[$APPNAME] -- $LINE Input parameter not known"
                 ;;
 	esac
 done
-#echo "export hex=1" >> selector.sh
-#echo "export uncert_radius=2" >> selector.sh
 echo "export uncert_type=position" >> selector.sh
-############# Default Values
+
+
+####################################################
+#
+# Set default values
+#
+####################################################
+
 AGE=0  ##AGE_TMP                    # if the oil has been released from long time and ithas evaporated for example
 GRID_SIZE=150.0 ##GRID_SIZE_TMP         # spatial resolution (m) of the oil tracer grid.
 SAT_DATA=NO #SAT_DATA_TMP        # for Point Source choose NO, for slick from satellite data choose YES
 CONTOUR_SLICK=NO  #CONTOUR_SLICK_TMP      # for Point Source choose NO, for manually slick contour insertion choose YES
 N_OS=1 #N_OS_TMP   # write the number of the slick to be simulated
+
+
+####################################################
+#
+# Set permissions
+#
+####################################################
+
 chmod 755 env.sh
 source env.sh
 chmod 755 selector.sh
 chmod 755 hour_step.sh
-#yyyymmdd="20${YEAR}${MONTH}${DAY}"
-#echo "today = $yyyymmdd"
-#yyyymmdd=`./jday ${yyyymmdd} 1`
-#DAY=`echo ${yyyymmdd} |cut -c7-8`
-#MONTH=`echo ${yyyymmdd} |cut -c5-6`
-#YEAR=`echo ${yyyymmdd} |cut -c3-4`
-#echo "Day new = $DAY, Date original = ${yyyymmdd}"
+
+
+####################################################
+#
+# Check the oil type
+#
+####################################################
+
 if [ $OIL == "NAME" ];then
 	OIL_TYPE=${OIL_TYPE//_/ }
-        echo $OIL_TYPE
 	OIL_TYPE=${OIL_TYPE//§/\&}
-        echo $OIL_TYPE
 	OIL_TYPE=${OIL_TYPE//^/\/}
-        echo $OIL_TYPE
 fi
+
+
+####################################################
+#
+# Generate medslik_inputfile.txt
+#
+####################################################
+
+echo "[$APPNAME] -- Generating medslik_inputfile.txt..."
 sed -e "s|SIM_NAME_TMP|$SIM_NAME|g"\
     -e "s|MODEL_TMP|${MODEL}|g"\
     -e "s|WIND_TMP|${WIND}|g"\
@@ -197,47 +246,31 @@ sed -e "s|SIM_NAME_TMP|$SIM_NAME|g"\
     -e "s|N_OS_TMP|$N_OS|g"\
      medslik_inputfile.txt.tmp > medslik_inputfile.txt.tmp1
 
-  if (( $(bc <<< "$START_LON > 0") == 1 ));then
+if (( $(bc <<< "$START_LON > 0") == 1 ));then
     sed -e "s|LON_DEGREE_TMP|$LON_DEGREE|g"\
         -e "s|LON_MINUTES_TMP|$LON_MINUTES|g"\
         medslik_inputfile.txt.tmp1 > medslik_inputfile.txt
-  else
+else
     sed -e "s|LON_DEGREE_TMP|-$LON_DEGREE|g"\
         -e "s|LON_MINUTES_TMP|-$LON_MINUTES|g"\
         medslik_inputfile.txt.tmp1 > medslik_inputfile.txt
-	lineNum=`cat env.sh |grep -n "LON_DEGREE=" | awk 'BEGIN {FS=":"} {print $1}'`
-        echo "export LON_DEGREE=$LON_DEGREE" 
-        Content=`echo "export LON_DEGREE=-$LON_DEGREE"`
-        sed -e "${lineNum}s/.*/${Content}/" env.sh > env.sh.tmp
-        lineNum=`cat env.sh |grep -n "LON_MINUTES=" | awk 'BEGIN {FS=":"} {print $1}'`
-        echo "export LON_MINUTES=$LON_MINUTES" 
-        Content=`echo "export LON_MINUTES=-$LON_MINUTES"`
-        sed -e "${lineNum}s/.*/${Content}/" env.sh.tmp > env.sh
-	rm env.sh.tmp
-	chmod 750 env.sh
+    lineNum=`cat env.sh |grep -n "LON_DEGREE=" | awk 'BEGIN {FS=":"} {print $1}'`
+    echo "[$APPNAME] -- export LON_DEGREE=$LON_DEGREE" 
+    Content=`echo "export LON_DEGREE=-$LON_DEGREE"`
+    sed -e "${lineNum}s/.*/${Content}/" env.sh > env.sh.tmp
+    lineNum=`cat env.sh |grep -n "LON_MINUTES=" | awk 'BEGIN {FS=":"} {print $1}'`
+    echo "[$APPNAME] -- export LON_MINUTES=$LON_MINUTES" 
+    Content=`echo "export LON_MINUTES=-$LON_MINUTES"`
+    sed -e "${lineNum}s/.*/${Content}/" env.sh.tmp > env.sh
+    rm env.sh.tmp
+    chmod 750 env.sh
+fi
+  
+####################################################
+#
+# exit gracefully
+#
+####################################################
 
-  fi
-
-
+echo "[$APPNAME] -- Elaboration completed!"
 exit 0
-# sim_name=simname;model=MYO1h;wind=ECMWF05;sim_length=0024;day=15;month=10;year=13;hour=08;minutes=00;lat_degree=42;lat_mintes=22.28;lon_degree=10;lon_minutes=55.50;duration=0036;spillrate=00006.80;oil=API;oiltype=17;var_2=1;var_3=0.00;var_10=2.0;var_14=90000;var_19=0.0000033;var_29=0.000008;var_39=150
-#####medslik_Input_File.txt.tmp example
-#SIM_NAME=SIM_NAME_TMP
-#MODEL=MODEL_TMP  # choose between MFS or AFS
-#WIND=WIND_TMP     # choose between ECMWF 0.5° or ECMWF 0.25°
-#SIM_LENGTH=SIM_LENGTH_TMP    # choose the simulation length (in hours, 4 characters)
-#DAY=DAY_TMP  #mettere il giorno successivo a quello in cui si vuole far partire la simulazione
-#MONTH=MONTH_TMP
-#YEAR=YEAR_TMP
-#HOUR=HOUR_TMP #orario in cui si vuole far partire la simulazione
-#MINUTES=MINUTES_TIME_TMP
-#LAT_DEGREE=LAT_DEGREE_TMP
-#LAT_MINUTES=LAT_MINUTES_TMP
-#LON_DEGREE=LON_DEGREE_TMP
-#LON_MINUTES=LON_MINUTES_TMP
-#DURATION=DURATION_TMP             # spill duration, in hours, 4 characters, if the spill is instantaneous type 0000
-#SPILL_RATE=SPILL_RATE_TMP        # spill rate in tons/hours,  if the spill is instantaneous write the total tons spilled
-#OIL=OIL_TMP
-#OIL_TYPE=OIL_TYPE_TMP
-#SAT_DATA=SAT_DATA_TMP        # for Point Source choose NO, for slick from satellite data choose YES
-###########end of medslik_Input_File.txt.tmp example
