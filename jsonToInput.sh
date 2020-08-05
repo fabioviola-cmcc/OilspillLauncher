05d67452ce835a987853069ed2f3bf804e501fa9
Fabio Viola <fabio.viola@cmcc.it>
2020-08-05 15:36:25 +0200

    Cleaned jsonToInput.sh from useless stuff

--- jsonToInput.sh
+++ jsonToInput.sh
@@ -17,7 +17,7 @@
 APPNAME=$0
 userName=`whoami`
 export homeDir="/users/home/tessa_gpfs1/${userName}"
-workDir=/work/tessa_gpfs2/${userName}/OILSPILL_DA/out
+workDir=/work/opa/${userName}/OILSPILL_DA/out
 export E_BADARGS="1"
 export INP_BADARGS="-1"   # code: 255
 export Curr_BADARGS="-2"  # code: 254
@@ -37,12 +37,15 @@ if [ $# -ne 1 ] ; then
    echo -e "[$APPNAME] -- Usage: ./`basename $0` 'sim_name=simname;model=MYO1h;wind=ECMWF05;sim_length=0024;day=15;month=10;year=13;hour=08;minutes=00;lat_degree=42;lat_mintes=22.28;lon_degree=10;lon_minutes=55.50;duration=0036;spillrate=00006.80;oil=API;oiltype=17;var_2=1;var_3=0.00;var_10=2.0;var_14=90000;var_19=0.0000033;var_29=0.000008;var_39=150' \n"
    exit $INP_BADARGS
 fi
+
+# parse arguments
 inputString=$1
 idDir=`echo ${inputString} | awk -F';' '{for(i = 1;i <= NF;i++){printf "%s\n", $i}}' | awk 'BEGIN{FS="="} ($1 == "id") {print $2}'`
 inputString=${inputString//\`/xxx}
-workingDir=${workDir}/${idDir}/witoil
+workingDir=${workDir}/${idDir}/witoil/EXE
 cd ${workingDir}
 echo "[$APPNAME] -- I'm in directory ${workingDir}"
+
 echo $1 > infile.txt
 
 ####################################################
@@ -52,131 +55,108 @@ echo $1 > infile.txt
 ####################################################
 
 echo "[$APPNAME] -- Parsing input string..."
-echo "${inputString}" | awk -F';' '{for(i = 1;i <= NF;i++){printf "%s\n", $i}}' | while read LINE; do # >>${workingDir}/file.txt # | while read LINE; do 
-	case $LINE in
-		"selector="*)
-                        SELECTOR=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export SELECTOR=${SELECTOR}" >> selector.sh
-		;;
-		"hex="*)
-			hex=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export hex=${hex}" >> selector.sh
-		;;
-		"uncert_radius="*)
-			uncert_radius=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export uncert_radius=${uncert_radius}" >> selector.sh
-		;;
-                "uncert_type="*)
-                        uncert_type=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export uncert_type=${uncert_type}" >> selector.sh
-                ;;
-		"id="*)
-			id=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export id=$id" >> env.sh
-		;;
-		"sim_name"*)
-			SIM_NAME=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export SIM_NAME=$SIM_NAME" >> env.sh
-		;;
-                "wind"*)
-                        WIND=`echo $LINE | awk -F'=' '{printf $2}'`
-			if [ $WIND == "ECMWF025" ];then
-			  WIND=ECMWF012
-			fi
-			echo "export WIND=$WIND" >> env.sh
-                ;;
-                "spillrate"*)
-                        SPILL_RATE=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export SPILL_RATE=$SPILL_RATE" >> env.sh
-                ;;
-                "model"*)
-                        MODEL=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export MODEL=$MODEL" >> env.sh
-                ;;
-                "minutes"*)
-                        MINUTES_TIME=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export MINUTES_TIME=$MINUTES_TIME" >> env.sh
-                ;;
-                "sim_length"*)
-                        SIM_LENGTH=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export SIM_LENGTH=$SIM_LENGTH" >> env.sh
-			echo "export SIM_LENGTH=$SIM_LENGTH" > hour_step.sh
-                ;;
-                "lon_min"*)
-                        LON_MINUTES=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export LON_MINUTES=$LON_MINUTES" >> env.sh
-                ;;
-                "hour"*)
-                        HOUR=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export HOUR=$HOUR" >> env.sh
-                ;;
-                "lon_deg"*)
-                        LON_DEGREE=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export LON_DEGREE=$LON_DEGREE" >> env.sh
-                ;;
-                "lat_deg"*)
-                        LAT_DEGREE=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export LAT_DEGREE=$LAT_DEGREE" >> env.sh
-                ;;
-                "oil="*)
-                        OIL=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export OIL=\"$OIL\"" >> env.sh
-                ;;
-                "duration"*)
-                        DURATION=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export DURATION=$DURATION" >> env.sh
-                ;;
-                "oiltype"*)
-                        OIL_TYPE=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export OIL_TYPE=\"$OIL_TYPE\"" >> env.sh
-                ;;
-                "month"*)
-                        MONTH=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export MONTH=$MONTH" >> env.sh
-                ;;
-                "year"*)
-                        YEAR=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export YEAR=$YEAR" >> env.sh
-                ;;
-                "day"*)
-                        DAY=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export DAY=$DAY" >> env.sh
-                ;;
-                "lat_min"*)
-                        LAT_MINUTES=`echo $LINE | awk -F'=' '{printf $2}'`
-			echo "export LAT_MINUTES=$LAT_MINUTES" >> env.sh
-                ;;
-		"var_"*)
-			lineNum=`echo $LINE | awk -F'=' '{printf $1}' | awk -F'_' '{printf $2}'`
-			echo $lineNum
-			Content=`echo $LINE | awk -F'=' '{printf $2}'`
-			sed -e "${lineNum}s/.*/${Content}/" medslik5.par > medslik5.par.tmp
-			mv medslik5.par.tmp medslik5.par
-		;;
-		"start_lon"*)
-			START_LON=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export START_LON=${START_LON}" >> env.sh
-                        echo "export START_LON=${START_LON}" >> selector.sh
-		;;
-                "start_lat"*)
-                        START_LAT=`echo $LINE | awk -F'=' '{printf $2}'`
-                        echo "export START_LAT=${START_LAT}" >> env.sh
-                        echo "export START_LAT=${START_LAT}" >> selector.sh
-                ;;
-		"plotStep"*)
-			lineNum=23
-			echo $lineNum
-			Content=`echo $LINE | awk -F'=' '{printf $2}'`
-			sed -e "${lineNum}s/.*/HOUR_STEP  = ${Content}/" medslik_plots/default_medslik_plots.ncl > medslik_plots/default_medslik_plots.ncl.tmp
-			mv medslik_plots/default_medslik_plots.ncl.tmp medslik_plots/default_medslik_plots.ncl 
-			echo "export HOUR_STEP=${Content}" >> hour_step.sh
-		;;
-                *)
-			echo "[$APPNAME] -- $LINE Input parameter not known"
-                ;;
-	esac
+echo "${inputString}" | awk -F';' '{for(i = 1;i <= NF;i++){printf "%s\n", $i}}' | while read LINE; do # >>${workingDir}/file.txt # | while read LINE; do
+    case $LINE in
+	"id="*)
+	    id=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export id=$id" >> env.sh
+	    ;;
+	"sim_name"*)
+	    SIM_NAME=`echo $LINE | awk -F'=' '{printf $2}'`
+            echo "export SIM_NAME=$SIM_NAME" >> env.sh
+	    ;;
+        "wind"*)
+            WIND=`echo $LINE | awk -F'=' '{printf $2}'`
+	    if [ $WIND == "ECMWF025" ];then
+		WIND=ECMWF012
+	    fi
+	    echo "export WIND=$WIND" >> env.sh
+            ;;
+        "spillrate"*)
+            SPILL_RATE=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export SPILL_RATE=$SPILL_RATE" >> env.sh
+            ;;
+        "model"*)
+            MODEL=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export MODEL=$MODEL" >> env.sh
+            ;;
+        "minutes"*)
+            MINUTES_TIME=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export MINUTES_TIME=$MINUTES_TIME" >> env.sh
+            ;;
+        "sim_length"*)
+            SIM_LENGTH=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export SIM_LENGTH=$SIM_LENGTH" >> env.sh
+	    echo "export SIM_LENGTH=$SIM_LENGTH" > hour_step.sh
+            ;;
+        "lon_min"*)
+            LON_MINUTES=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export LON_MINUTES=$LON_MINUTES" >> env.sh
+            ;;
+        "hour"*)
+            HOUR=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export HOUR=$HOUR" >> env.sh
+            ;;
+        "lon_deg"*)
+            LON_DEGREE=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export LON_DEGREE=$LON_DEGREE" >> env.sh
+            ;;
+        "lat_deg"*)
+            LAT_DEGREE=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export LAT_DEGREE=$LAT_DEGREE" >> env.sh
+            ;;
+        "oil="*)
+            OIL=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export OIL=\"$OIL\"" >> env.sh
+            ;;
+        "duration"*)
+            DURATION=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export DURATION=$DURATION" >> env.sh
+            ;;
+        "oiltype"*)
+            OIL_TYPE=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export OIL_TYPE=\"$OIL_TYPE\"" >> env.sh
+            ;;
+        "month"*)
+            MONTH=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export MONTH=$MONTH" >> env.sh
+            ;;
+        "year"*)
+            YEAR=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export YEAR=$YEAR" >> env.sh
+            ;;
+        "day"*)
+            DAY=`echo $LINE | awk -F'=' '{printf $2}'`
+            echo "export DAY=$DAY" >> env.sh
+            ;;
+        "lat_min"*)
+            LAT_MINUTES=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export LAT_MINUTES=$LAT_MINUTES" >> env.sh
+            ;;
+	"var_"*)
+	    lineNum=`echo $LINE | awk -F'=' '{printf $1}' | awk -F'_' '{printf $2}'`
+	    Content=`echo $LINE | awk -F'=' '{printf $2}'`
+	    sed -e "${lineNum}s/.*/${Content}/" medslik5.par > medslik5.par.tmp
+	    mv medslik5.par.tmp medslik5.par
+	    ;;
+	"start_lon"*)
+	    START_LON=`echo $LINE | awk -F'=' '{printf $2}'`
+            echo "export START_LON=${START_LON}" >> env.sh
+	    ;;
+        "start_lat"*)
+            START_LAT=`echo $LINE | awk -F'=' '{printf $2}'`
+            echo "export START_LAT=${START_LAT}" >> env.sh
+            ;;
+	"plotStep"*)
+	    lineNum=23
+	    Content=`echo $LINE | awk -F'=' '{printf $2}'`
+	    echo "export HOUR_STEP=${Content}" >> hour_step.sh
+	    ;;
+        *)
+	    echo "[$APPNAME] -- $LINE Input parameter not known"
+            ;;
+    esac
 done
-echo "export uncert_type=position" >> selector.sh
 
 
 ####################################################
@@ -200,7 +180,6 @@ N_OS=1 #N_OS_TMP   # write the number of the slick to be simulated
 
 chmod 755 env.sh
 source env.sh
-chmod 755 selector.sh
 chmod 755 hour_step.sh
 
 
@@ -224,6 +203,7 @@ fi
 ####################################################
 
 echo "[$APPNAME] -- Generating medslik_inputfile.txt..."
+
 sed -e "s|SIM_NAME_TMP|$SIM_NAME|g"\
     -e "s|MODEL_TMP|${MODEL}|g"\
     -e "s|WIND_TMP|${WIND}|g"\
